ebd2dfa88edfeeb165b544059727034236dfe866
shihab <shihab.zn4@gmail.com>
2026-05-26 00:45:36 +0600

    chore: add description comment to DashboardWelcome component

--- src/components/dashboard-welcome.tsx
+++ src/components/dashboard-welcome.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { WelcomeModal } from "./welcome-modal";
 
+// DashboardWelcome wrapper that triggers the welcome modal check on dashboard load
 export function DashboardWelcome() {
   useEffect(() => {
     // This component will trigger the welcome modal check on dashboard load
