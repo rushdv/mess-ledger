4cfd5e0746dee1a2bac585d428de5eef0eb7b9fa
shihab <shihab.zn4@gmail.com>
2026-05-26 00:45:33 +0600

    chore: document cn utility in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Combines tailwind classes safely using clsx and tailwind-merge
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
