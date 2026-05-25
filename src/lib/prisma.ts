dd08f2349a8da0095a188832d6bba2872ac59b24
shihab <shihab.zn4@gmail.com>
2026-05-26 00:45:32 +0600

    chore: add comments to prisma client helper

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
+// Prevent multiple instances of Prisma Client in development mode
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
